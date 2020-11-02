ffde0d507afaab949ecac24f2f6cd5faea4df18d
Marcus Jensen <marcusproman0507@gmail.com>
2020-11-02 08:42:35 +0100

    0.2.6.5

--- TouchPortal Plugin Helper/Edit.xaml.cs
+++ TouchPortal Plugin Helper/Edit.xaml.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 using System.IO;
 using System.Windows;
+using System.Diagnostics;
 using System.Windows.Media;
 using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Windows.Navigation;
-using System.Collections.Generic;
-using System.Security.Authentication.ExtendedProtection;
-using System.Diagnostics;
 
 namespace TouchPortal_Plugin_Helper
 {
@@ -17,60 +15,61 @@ namespace TouchPortal_Plugin_Helper
         {
             InitializeComponent();
         }
-
+        #region IMPORT
         //Drag and drop
-        private async void TextBlock_Drop(object sender, DragEventArgs e)
+        private /*async*/ void TextBlock_Drop(object sender, DragEventArgs e)
         {
             if (e.Data.GetDataPresent(DataFormats.FileDrop))
             {
-                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                MessageBox.Show("The import feature does not work atm. Hang tight\n\n-tsgsOFFICIAL", "TouchPortal Plugin Helper", MessageBoxButton.OK, MessageBoxImage.Error);
+                //string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
 
 
-                if (files.Length == 1) //Only allow 1 file
-                {
-                    if (CheckFileType(files[0]) == "tp") //Only allow files of type: 'tp'
-                    {
-                        DragAndDropTextblock.Text = "Checking for errors..";
-                        if (CheckDotTP())
-                        {
-                            //Delete Backup, since we're editing a new file!
-                            try
-                            {
-                                Directory.Delete($@"{Path.GetTempPath()}\TouchPortalPluginHelper", true);
-                            }
-                            catch (Exception) { }
+                //if (files.Length == 1) //Only allow 1 file
+                //{
+                //    if (CheckFileType(files[0]) == "tp") //Only allow files of type: 'tp'
+                //    {
+                //        DragAndDropTextblock.Text = "Checking for errors..";
+                //        if (CheckDotTP())
+                //        {
+                //            //Delete Backup, since we're editing a new file!
+                //            try
+                //            {
+                //                Directory.Delete($@"{Path.GetTempPath()}\TouchPortalPluginHelper", true);
+                //            }
+                //            catch (Exception) { }
 
-                            DragAndDropTextblock.Text = "Looking good! Just adding everything to the program ;)";
+                //            DragAndDropTextblock.Text = "Looking good! Just adding everything to the program ;)";
 
-                            AddFromDotTP();
-                            NavigationService.Navigate(new Main()); //Navigate to main again, once the 'entry.tp' file has been loaded all in
-                        }
-                        else
-                        {
-                            DragAndDropTextblock.Foreground = Brushes.Red;
-                            DragAndDropTextblock.Text = "The file is not valid";
-                            await Task.Delay(1250);
-                            DragAndDropTextblock.Foreground = Brushes.GhostWhite;
-                            DragAndDropTextblock.Text = "Drag and drop your 'entry.tp' file here";
-                        }
-                    }
-                    else
-                    {
-                        DragAndDropTextblock.Foreground = Brushes.Red;
-                        DragAndDropTextblock.Text = $"Only '.tp' files! You can't use a \"{CheckFileType(files[0])}\" file";
-                        await Task.Delay(1250);
-                        DragAndDropTextblock.Foreground = Brushes.GhostWhite;
-                        DragAndDropTextblock.Text = "Drag and drop your 'entry.tp' file here";
-                    }
-                }
-                else
-                {
-                    DragAndDropTextblock.Foreground = Brushes.Red;
-                    DragAndDropTextblock.Text = "You can only edit 1 at a time";
-                    await Task.Delay(1250);
-                    DragAndDropTextblock.Foreground = Brushes.GhostWhite;
-                    DragAndDropTextblock.Text = "Drag and drop your 'entry.tp' file here";
-                }
+                //            AddFromDotTP();
+                //            NavigationService.Navigate(new Main()); //Navigate to main again, once the 'entry.tp' file has been loaded all in
+                //        }
+                //        else
+                //        {
+                //            DragAndDropTextblock.Foreground = Brushes.Red;
+                //            DragAndDropTextblock.Text = "The file is not valid";
+                //            await Task.Delay(1250);
+                //            DragAndDropTextblock.Foreground = Brushes.GhostWhite;
+                //            DragAndDropTextblock.Text = "Drag and drop your 'entry.tp' file here";
+                //        }
+                //    }
+                //    else
+                //    {
+                //        DragAndDropTextblock.Foreground = Brushes.Red;
+                //        DragAndDropTextblock.Text = $"Only '.tp' files! You can't use a \"{CheckFileType(files[0])}\" file";
+                //        await Task.Delay(1250);
+                //        DragAndDropTextblock.Foreground = Brushes.GhostWhite;
+                //        DragAndDropTextblock.Text = "Drag and drop your 'entry.tp' file here";
+                //    }
+                //}
+                //else
+                //{
+                //    DragAndDropTextblock.Foreground = Brushes.Red;
+                //    DragAndDropTextblock.Text = "You can only edit 1 at a time";
+                //    await Task.Delay(1250);
+                //    DragAndDropTextblock.Foreground = Brushes.GhostWhite;
+                //    DragAndDropTextblock.Text = "Drag and drop your 'entry.tp' file here";
+                //}
             }
         }
 
@@ -95,7 +94,7 @@ namespace TouchPortal_Plugin_Helper
                 string FileName = _temp[0].Substring(_temp[0].LastIndexOf('\\') + 1); //Get the filename
                 string Extension = _temp[1]; //Get the extension
 
-                return Extension; //Return the extension
+                return Extension.ToLower(); //Return the extension
             }
             catch (Exception)
             {
@@ -103,7 +102,6 @@ namespace TouchPortal_Plugin_Helper
             }
         }
 
-        #region IMPORT
         //Check if the .tp file is valid
         private bool CheckDotTP()
         {
--- TouchPortal Plugin Helper/Properties/AssemblyInfo.cs
+++ TouchPortal Plugin Helper/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.2.6.4")]
+[assembly: AssemblyFileVersion("0.2.6.5")]
 [assembly: NeutralResourcesLanguage("en")]
